b98450ff88a6a19ec4dc78123068639484b938d1
Gabriel Castro <gabrielcastro.xy@gmail.com>
2022-06-18 11:10:34 -0300

    Creating initial structure

---
+++ src/App.styled.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Header = styled.header`
+  background: blue;
+  width: auto;
+  height: 150px;
+`;
+
+export const HeaderText = styled.h1`
+  margin: 0;
+  padding: 0;
+  padding-top: 30px;
+  text-align: center;
+  color: #fff;
+`;
+
+export const Body = styled.div`
+  margin: auto;
+  max-width: 980px;
+  margin-bottom: 50px;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as Styled from './App.styled';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return(
+    <Styled.Container>
+      <Styled.Header>
+        <Styled.HeaderText>Finance System</Styled.HeaderText>
+      </Styled.Header>
+
+      <Styled.Body>
+        ...
+      </Styled.Body>
+    </Styled.Container>
+  )
 }
 
 export default App;
