e29263d58f0f5761f3c3c1954f8968fd51cfafba
Gabriel Castro <gabrielcastro.xy@gmail.com>
2022-11-12 11:05:40 -0300

    feat: Changing categories values

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,8 +1,6 @@
 import {Category} from '../types/Category';
 
 export const categories: Category = {
-  food: {title: 'Alimentação', color: 'blue', expense: true},
-  rent: {title: 'Aluguel', color: 'brown', expense: true},
-  recipe: {title: 'Fatura', color: 'red', expense: true},
-  salary: {title: 'Salário', color: 'green', expense: false},
+  input: {title: 'Entrada', color: 'green', expense: false},
+  output: {title: 'Saída', color: 'red', expense: true},
 }
--- src/data/items.ts
+++ src/data/items.ts
@@ -1,8 +1,6 @@
 import { Item } from '../types/Item';
 
 export const items: Item[] = [
-    { date: new Date(2021, 4, 30), category: 'food', title: 'Teste', value: 1 },
-    { date: new Date(2021, 4, 30), category: 'rent', title: 'Teste', value: 1 },
-    { date: new Date(2021, 4, 30), category: 'salary', title: 'Teste', value: 1 },
-    { date: new Date(2021, 4, 30), category: 'recipe', title: 'Teste', value: 1 },
+    { date: new Date(2021, 4, 30), category: 'input', title: 'Teste', value: 1 },
+    { date: new Date(2021, 4, 30), category: 'output', title: 'Teste', value: 1 },
 ];
